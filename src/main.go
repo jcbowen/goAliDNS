fd8d30ecd81ac89a5b5821f85383b726ba1bfb86
Bowen <bowen@jiuchet.com>
2021-09-18 01:53:05 +0800

    初始化

---
+++ src/main.go
@@ -0,0 +1,162 @@
+// This file is auto-generated, don't edit it. Thanks.
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	alidns20150109 "github.com/alibabacloud-go/alidns-20150109/v2/client"
+	openapi "github.com/alibabacloud-go/darabonba-openapi/client"
+	"github.com/alibabacloud-go/tea/tea"
+	"github.com/thedevsaddam/gojsonq"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
+)
+
+// 获取json格式的ip信息
+func getCurrenJsonIp() string {
+	res, err := http.Get("https://ipv6.jsonip.com")
+	if err != nil {
+		res, err = http.Get("https://jsonip.com")
+		if err != nil {
+			return ""
+		}
+	}
+	robots, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		return ""
+	}
+	return string(robots)
+}
+
+// ParseIP
+// 0: invalid ip
+// 4: ipv4
+// 6: ipv6
+func ParseIP(s string) (net.IP, int) {
+	ip := net.ParseIP(s)
+	if ip == nil {
+		return nil, 0
+	}
+	for i := 0; i < len(s); i++ {
+		switch s[i] {
+		case '.':
+			return ip, 4
+		case ':':
+			return ip, 6
+		}
+	}
+	return nil, 0
+}
+
+// CreateClient
+/**
+ * 使用AK&SK初始化账号Client
+ * @param accessKeyId
+ * @param accessKeySecret
+ * @return Client
+ * @throws Exception
+ */
+func CreateClient(accessKeyId *string, accessKeySecret *string) (_result *alidns20150109.Client, _err error) {
+	config := &openapi.Config{
+		// 您的AccessKey ID
+		AccessKeyId: accessKeyId,
+		// 您的AccessKey Secret
+		AccessKeySecret: accessKeySecret,
+	}
+	// 访问的域名
+	config.Endpoint = tea.String("alidns.cn-shenzhen.aliyuncs.com")
+	_result = &alidns20150109.Client{}
+	_result, _err = alidns20150109.NewClient(config)
+	return _result, _err
+}
+
+func _main(args []*string) (_err error) {
+	file, fErr := ioutil.ReadFile("config/config.json")
+	if fErr != nil {
+		return fErr
+	}
+
+	fileData := string(file)
+	configGojsonq := gojsonq.New().FromString(fileData)
+	accessKeyId := configGojsonq.Find("aliOpenApi.accessKeyId").(string)
+	accessKeySecret := configGojsonq.Reset().Find("aliOpenApi.accessKeySecret").(string)
+
+	client, _err := CreateClient(tea.String(accessKeyId), tea.String(accessKeySecret))
+	if _err != nil {
+		return _err
+	}
+
+	describeSubDomainRecordsRequest := &alidns20150109.DescribeSubDomainRecordsRequest{
+		SubDomain: tea.String("ipv6-cname.jiuchet.shop"),
+	}
+
+	// 获取子域名解析记录列表
+	result, _err := client.DescribeSubDomainRecords(describeSubDomainRecordsRequest)
+	if _err != nil {
+		return _err
+	}
+
+	// 获取当前IP
+	var ipVersion int
+	jsonString := getCurrenJsonIp()
+	currenIp := gojsonq.New().FromString(jsonString).Find("ip").(string)
+	_, ipVersion = ParseIP(currenIp)
+
+	recordString := result.Body.DomainRecords.Record[0].String()
+	type con struct {
+		Status     string
+		Type       string
+		Weight     int
+		Value      string
+		TTL        int
+		Line       string
+		RecordId   string
+		RR         string
+		DomainName string
+		Locked     bool
+	}
+	record := &con{}
+	_err = json.Unmarshal([]byte(recordString), record)
+
+	fmt.Println("当前主机的IP地址为：", currenIp)
+	fmt.Println("当前解析的IP地址为：", record.Value)
+
+	fmt.Println("查询子域名解析记录如下：")
+	fmt.Println(recordString)
+
+	// 如果IP发生了变化
+	if currenIp != "" && currenIp != record.Value {
+		var Type = "AAAA"
+		if ipVersion == 4 {
+			Type = "AAA"
+		}
+		updateDomainRecordRequest := &alidns20150109.UpdateDomainRecordRequest{
+			RecordId: tea.String(record.RecordId),
+			RR:       tea.String(record.RR),
+			Type:     tea.String(Type),
+			Value:    tea.String(currenIp),
+		}
+
+		updateResult, __err := client.UpdateDomainRecord(updateDomainRecordRequest)
+		if __err != nil {
+			return __err
+		}
+
+		fmt.Println("更新解析成功，返回信息如下：")
+		fmt.Println(updateResult.Body.String())
+	} else {
+		fmt.Println("IP未发生变化，不执行更新")
+	}
+
+	return _err
+}
+
+func main() {
+	err := _main(tea.StringSlice(os.Args[1:]))
+	if err != nil {
+		panic(err)
+	}
+}
